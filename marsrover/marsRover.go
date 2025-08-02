da825d493803c3cabe0f25daf397594f35650d33
SWARATHMICA <swarathmica@gmail.com>
2025-08-02 16:31:22 +0530

    Restrict Plateau Creation with values x<1

--- marsrover/marsRover.go
+++ marsrover/marsRover.go
@@ -1,9 +1,14 @@
 package marsrover
 
+import "errors"
+
 type Plateau struct {
 	x, y int
 }
 
-func NewPlateau(x, y int) Plateau {
-	return Plateau{x: x, y: y}
+func NewPlateau(x, y int) (*Plateau, error) {
+	if x < 1 {
+		return nil, errors.New("Cannot create plateau with x<1")
+	}
+	return &Plateau{x: x, y: y}, nil
 }
--- marsrover/marsRover_test.go
+++ marsrover/marsRover_test.go
@@ -4,9 +4,18 @@ import "testing"
 
 func TestNewPlateau(t *testing.T) {
 	x, y := 5, 5
-	plateau := NewPlateau(x, y)
+	plateau, _ := NewPlateau(x, y)
 
 	if plateau.x != 5 || plateau.y != 5 {
 		t.Errorf("Expected (5, 5), got (%d, %d)", plateau.x, plateau.y)
 	}
+}
+
+func TestCannotCreatePlateauWithxLessThan1(t *testing.T) {
+	x, y := -1, 5
+	_, err := NewPlateau(x, y)
+	if err == nil {
+		t.Errorf("Cannot create Plateau With negative x")
+	}
+
 }
